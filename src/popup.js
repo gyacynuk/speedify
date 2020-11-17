2dcb5bad2cdd5664983f91798549353c17ba572f
Griffin <gyacynuk@gmail.com>
2020-11-17 03:48:55 -0500

    Extract some constants

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,11 @@
 import { getStorageData, setStorageData, getCurrentTab } from './modules/utils';
 
 
+const MAX_SPEED = 300;
+const MIN_SPEED = 10;
+const NORMAL_SPEED = 100;
+const SPEED_INCREMENT = 10;
+
 const setDisabled = state => {
   if (state) {
     document.querySelectorAll('.enabled').forEach(el => (el.className = el.className.replace('enabled', 'disabled')));
@@ -21,13 +26,13 @@ const adjustPlaybackSpeed = (label, increment) => async function (e) {
   const { id } = await getCurrentTab();
   const { tabData } = await getStorageData('tabData');
   let isActive = tabData[id] ? tabData[id]['isActive'] : false;
-  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : 100;
+  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : NORMAL_SPEED;
 
   if (!isActive) return;
 
   let newSpeed = playbackSpeed + increment
-  if (newSpeed < 10) newSpeed = 10;
-  if (newSpeed > 300) newSpeed = 300;
+  if (newSpeed < MIN_SPEED) newSpeed = MIN_SPEED;
+  if (newSpeed > MAX_SPEED) newSpeed = MAX_SPEED;
 
   setStorageData({ tabData: { ...tabData, [id]: { playbackSpeed: newSpeed, isActive: isActive } } });
   label.innerHTML = newSpeed + '%'
@@ -38,10 +43,10 @@ const toggleIsActive = () => async function (e) {
   const { id } = await getCurrentTab();
   const { tabData } = await getStorageData('tabData');
   let isActive = tabData[id] ? tabData[id]['isActive'] : false;
-  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : 100;
+  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : NORMAL_SPEED;
 
   setStorageData({ tabData: { ...tabData, [id]: { playbackSpeed: playbackSpeed, isActive: !isActive } } });
-  sendPlaybackSpeedMessage(isActive ? 100 : playbackSpeed);
+  sendPlaybackSpeedMessage(isActive ? NORMAL_SPEED : playbackSpeed);
   setDisabled(isActive);
 }
 
@@ -49,15 +54,15 @@ document.addEventListener('DOMContentLoaded', async function () {
   const { id } = await getCurrentTab();
   const { tabData } = await getStorageData('tabData');
   let isActive = tabData[id] ? tabData[id]['isActive'] : false;
-  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : 100;
+  let playbackSpeed = tabData[id] ? tabData[id]['playbackSpeed'] : NORMAL_SPEED;
 
   let playbackSpeedLabel = document.getElementById('playbackSpeedLabel');
   playbackSpeedLabel.innerHTML = playbackSpeed + '%'
 
   let minusButton = document.getElementById('minus');
-  minusButton.addEventListener('click', adjustPlaybackSpeed(playbackSpeedLabel, -10));
+  minusButton.addEventListener('click', adjustPlaybackSpeed(playbackSpeedLabel, -SPEED_INCREMENT));
   let plusButton = document.getElementById('plus');
-  plusButton.addEventListener('click', adjustPlaybackSpeed(playbackSpeedLabel, 10));
+  plusButton.addEventListener('click', adjustPlaybackSpeed(playbackSpeedLabel, SPEED_INCREMENT));
 
   let toggle = document.getElementById('toggle');
   let toggleLabel = document.getElementById('toggleLabel');
